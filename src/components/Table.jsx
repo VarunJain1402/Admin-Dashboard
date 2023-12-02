e5b1eb817f06f7dd13b357f58faed30e414dcb79
Varun <varunjain1402@gmail.com>
2023-12-02 23:31:32 +0530

    displayed data in table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,55 +1,63 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+import { DataGrid } from '@mui/x-data-grid'
+import axios from 'axios'
+import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'id', headerName: 'ID', width: 70, sortable: false },
+  { field: 'Name', headerName: 'Name', width: 130, sortable: false , editable: true},
+  { field: 'Email', headerName: 'Email', width: 180, sortable: false , editable: true},
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
+    field: 'Role',
+    headerName: 'Role',
     description: 'This column has a value getter and is not sortable.',
     sortable: false,
     width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    // valueGetter: params =>
+    //   `${params.row.firstName || ''} ${params.row.lastName || ''}`
   },
-];
+  { field: 'actions', headerName: 'Actions', width: 130 }
+]
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
 
- function Table() {
+function Table () {
+  const [info, setInfo] = useState([])
+  useEffect(() => {
+    axios
+      .get(
+        'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json'
+      )
+      .then(response => {
+        setInfo(response.data)
+        console.log(response.data)
+      })
+  },[])
+
+  const rows = info.map((info) => {
+    return({
+        id: info.id , 
+        Name:info.name , 
+        Email:info.email , 
+        Role: info.role ,
+        // actions: {<><DeleteSharpIcon /></>}
+    })
+  })
   return (
-    <div style={{ height: 400, width: '100%', marginTop:'5%' }}>
+    <div style={{ width: '100%', marginTop: '5%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 10 },
-          },
+            paginationModel: { page: 0, pageSize: 10 }
+          }
         }}
-        pageSizeOptions={[5,10]}
+        pageSizeOptions={[10]}
         checkboxSelection
       />
     </div>
-  );
+  )
 }
 
 export default Table
